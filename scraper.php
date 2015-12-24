02e64703652cc36bd65ffcca704c2fecca935b99
Philip Ashlock <phil@ashlock.us>
2015-12-23 19:03:41 -0500

    SQLite test

--- scraper.php
+++ scraper.php
@@ -24,4 +24,40 @@
 // All that matters is that your final data is written to an SQLite database
 // called "data.sqlite" in the current working directory which has at least a table
 // called "data".
+
+$db = new PDO('sqlite:data.sqlite');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+try {
+  $db->query('CREATE TABLE data(
+    guid VARCHAR(100),
+    description TEXT,
+    title VARCHAR(100),
+    article_timestamp VARCHAR(10),
+    PRIMARY KEY (guid))');
+} catch (Exception $e) {
+}
+
+
+$articles = array(array('guid' => "3", 'description' => 'this is a test', 'title' => 'this is a title', 'article_timestamp' => 'date'));
+
+
+foreach ($articles as $article) {
+  $exists = $db->query("SELECT * FROM data WHERE guid = " . $db->quote($article->guid))->fetchObject();
+  if (!$exists) {
+    $sql = "INSERT INTO data(guid, description, title, article_timestamp) VALUES(:guid, :description, :title, :article_timestamp)";
+  } else {
+    $sql = "UPDATE data SET description = :description, article_timestamp = :article_timestamp WHERE guid = :guid";
+  }
+  $statement = $db->prepare($sql);
+    $statement->execute(array(
+    ':guid' => $article['guid'], 
+    ':description' => $article['description'],
+    ':title' => $article['title'],
+    ':article_timestamp' => $article['article_timestamp']
+  ));
+}
+
+
+
 ?>
